9eaf64bb81ea860c691454ef3426cb301790227a
Sony B A <sonyshaiva444@gmail.com>
2026-07-29 16:00:47 +0530

    Checking palindrome of a number

--- 3palindrome.py
+++ 3palindrome.py
@@ -17,6 +17,4 @@ class Solution(object):
             rev = (rev * 10) + (n % 10)
             n = n // 10
 
-        if rev == x:
-            return True
-        return False
+        return rev == x
