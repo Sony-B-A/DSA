edbdd2b627355a80fecdc44281d98b3c20ed2701
Sony B A <sonyshaiva444@gmail.com>
2026-08-05 22:07:04 +0530

    print from n to 1

---
+++ 9nto1.py
@@ -0,0 +1,14 @@
+# GFG
+'''
+Print n to 1 without loop
+Difficulty: Easy
+Print numbers from n to 1 (space separated) without the help of loops.
+'''
+
+class Solution:
+    def printNos(self, n):
+        # Code here
+        if n ==0:
+            return
+        print(n, end=' ')
+        self.printNos(n-1)
